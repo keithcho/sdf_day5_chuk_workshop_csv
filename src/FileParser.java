065a4e99c022dcd0334544be7845a568fac509a3
Keith <135153337+keithcho@users.noreply.github.com>
2024-10-16 17:09:55 +0800

    update fileparser

--- src/FileParser.java
+++ src/FileParser.java
@@ -3,7 +3,9 @@ import java.util.*;
 
 public class FileParser {
     private Reader reader;
-    private Map<String, String[]> cellData;
+    private Map<String, ArrayList<Object>> cellData;
+    private Integer lowestRating;
+    private Integer highestRating;
 
     public FileParser(Reader reader) {
         this.reader = reader;
@@ -12,17 +14,22 @@ public class FileParser {
 
     public void readFile() throws IOException {
         BufferedReader br = new BufferedReader(reader);
-        String header;
         String line;
         int linesRead = 0;
 
+        // Allocate first line to headers
         String[] cellHeader = br.readLine().split(",");
         linesRead += 1;
 
         while ((line = br.readLine()) != null) {
+            ArrayList<String>
             String[] cellRow = line.split(",");
-            String[] appData = {cellRow[1], cellRow[2]};
-            cellData.put(cellRow[0], appData);
+            // cellRow[0] = name, cellRow[1] = category, cellRow[2] = rating
+            String[] categoryData = {cellRow[0], cellRow[2]};
+            if (cellData.containsKey(cellRow[0])) {
+
+            }
+            // cellData.put(cellRow[1], categoryData);
             linesRead += 1;
         }
 
